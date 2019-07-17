93f9bbc843b9f8e22f933e8df0ebe5c9192556e0
Cory Hedeen <hedeencory@gmail.com>
2019-07-17 18:06:57 -0400

    Render components

--- src/components/Image.js
+++ src/components/Image.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Image(props){
   return(
     <div className="image">
-      <img src={props.image} alt="NASA photo of the day"/>
+      <img src={props.image.url} alt="NASA photo of the day"/>
     </div>
   )
 }
---
+++ src/components/Title.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function Title(props){
+  return(
+    <div className="title">
+      <h2>{props.title.title}</h2>
+    </div>
+  )
+}
+export default Title;
