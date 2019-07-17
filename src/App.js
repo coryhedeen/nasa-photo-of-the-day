55f569c17892978bb89f6dbeba42da5623b1fb50
Cory Hedeen <hedeencory@gmail.com>
2019-07-17 18:06:27 -0400

    Render description and title

--- src/App.js
+++ src/App.js
@@ -2,14 +2,15 @@ import React, { useEffect, useState } from "react";
 import "./App.css";
 import axios from 'axios';
 import Image from './components/Image';
-
+import Title from './components/Title';
+import Description from './components/Description';
 function App() {
 const [photo, setPhoto] = useState([]);
 
 useEffect(() => {
   axios.get("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=2019-07-15")
-      .then(res => {
-        setPhoto(res.data.url)
+      .then(res => { console.log(res.data);
+        setPhoto(res.data)
       })
       .catch(err => console.log(err))
 }, [])
@@ -18,7 +19,9 @@ useEffect(() => {
   return (
     <div className="App">
       <p>
+        <Title title={photo}/>
         <Image image={photo}/>
+        <Description des={photo} />
       </p>
     </div>
   );
