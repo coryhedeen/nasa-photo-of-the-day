0c22bed5562b147db4daf854baaa4648e85bb5ff
Cory Hedeen <hedeencory@gmail.com>
2019-07-17 17:38:16 -0400

    Build useEffect function

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
+import axios from 'axios';
+import Image from './components/Image';
 
 function App() {
+const [photo, setPhoto] = useState([]);
+
+useEffect(() => {
+  axios.get("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=2019-07-15")
+      .then(res => console.log(res.data))
+      .catch(err => console.log(err))
+}, [])
+
+
   return (
     <div className="App">
       <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun 🚀!
+        <Image image={photo}/>
       </p>
     </div>
   );
