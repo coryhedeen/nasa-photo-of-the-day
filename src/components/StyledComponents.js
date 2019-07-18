2c935d9ce6ea07bd031a36dfa98d2acf4302b6e3
Cory Hedeen <hedeencory@gmail.com>
2019-07-18 15:21:05 -0400

    Add styling to description paragraph

--- src/components/StyledComponents.js
+++ src/components/StyledComponents.js
@@ -3,3 +3,9 @@ import styled from 'styled-components';
 export const Sun = styled.img`
   width: 50%;
 `;
+
+export const Explain = styled.p`
+  width: 50%;
+  text-align: justify;
+  margin-left: 250px;
+`;
