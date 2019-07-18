97afac884a53f22616ec30bad12737da17d8c7aa
Cory Hedeen <hedeencory@gmail.com>
2019-07-18 15:35:25 -0400

    Add three styled components and implement them

--- src/components/StyledComponents.js
+++ src/components/StyledComponents.js
@@ -2,10 +2,16 @@ import styled from 'styled-components';
 
 export const Sun = styled.img`
   width: 50%;
+  border: 10px solid black;
 `;
 
 export const Explain = styled.p`
   width: 50%;
   text-align: justify;
   margin-left: 250px;
+  font-family: 'Literata', serif;
+`;
+
+export const Header = styled.h1`
+  font-family: 'Literata', serif;
 `;
